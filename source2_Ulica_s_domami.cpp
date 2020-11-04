37da58785a79b2669693cbff07d890274bb93661
Tomas Homola <homola.tom6@gmail.com>
2020-11-04 19:09:13 +0100

    Prerobenie asi hotove, cele nove riesenie je v source2_Ulica_s_domami

--- source2_Ulica_s_domami.cpp
+++ source2_Ulica_s_domami.cpp
@@ -38,7 +38,7 @@ private:
 
 public:
 	FamilyHouse() : Building(0, 0) { this->houseArea = 0; this->gardenArea = 0; } // funkcie setNumber() a setFloors() su zdedene z triedy Building
-	FamilyHouse(int number, int floors, int houseArea, int gardenArea) : Building(0, 0) { this->houseArea = houseArea; this->gardenArea = gardenArea; }
+	FamilyHouse(int number, int floors, int houseArea, int gardenArea) : Building(number, floors) { this->houseArea = houseArea; this->gardenArea = gardenArea; }
 	~FamilyHouse() {}//{ std::cout << "destruktor family house\n"; }
 
 	// funkcie na vratenie premennych
@@ -164,14 +164,115 @@ public:
 	Street() {};
 	Street(string streetName, string cityName);
 
-	Building** familyHouses = nullptr;
-	Building** blocksOfFlats = nullptr;
-	Building** officeBuildings = nullptr;
-	Building** factories = nullptr;
+	FamilyHouse** familyHouses = nullptr;
+	BlockOfFlats** blocksOfFlats = nullptr;
+	OfficeBuilding** officeBuildings = nullptr;
+	Factory** factories = nullptr;
+
+
+	string getStreetName() { return streetName; }
+	string getCityName() { return cityName; }
+
+	void setStreetName(string newStreetName) { this->streetName = newStreetName; }
+	void setCityName(string newCityName) { this->cityName = newCityName; }
+
+	void generateBuildings();
+	void printInfo();
 };
 
 Street::Street(string streetName, string cityName)
 {
 	this->streetName = streetName;
 	this->cityName = cityName;
+}
+
+void Street::generateBuildings()
+{
+	int tempFloors = 0, tempEmplyees = 0;
+
+	numOfFamilyHouses = rand() % 16 + 5; // 5-20
+	numOfBlocksOfFlats = rand() % 8 + 3; // 3-10
+	numOfOfficeBuildings = rand() % 5 + 2; // 2-6
+	numOfFactories = rand() % 3 + 1; // 1-3
+
+	familyHouses = new FamilyHouse * [numOfFamilyHouses];
+	blocksOfFlats = new BlockOfFlats * [numOfBlocksOfFlats];
+	officeBuildings = new OfficeBuilding * [numOfOfficeBuildings];
+	factories = new Factory * [numOfFactories];
+
+	for (int i = 0; i < numOfFamilyHouses; i++)
+	{
+		familyHouses[i] = new FamilyHouse(i + 7001, rand() % 2 + 1, rand() % 101 + 50, rand() % 101 + 100);
+	}
+
+	for (int i = 0; i < numOfBlocksOfFlats; i++)
+	{
+		tempFloors = rand() % 13 + 3; // pomocna premenna, kde bude ulozeny pocet poschodi a to sa potom pouzije na vypocet celkoveho poctu bytov v block of flats
+		blocksOfFlats[i] = new BlockOfFlats(i + 7101, tempFloors, tempFloors * (rand() % 4 + 1)); // floors: 3-15, flats: 1-4
+	}
+
+	for (int i = 0; i < numOfOfficeBuildings; i++)
+	{
+		tempFloors = rand() % 20 + 1; // floors: 1-20
+		tempEmplyees = tempFloors * (rand() % 16 + 10); // employees: 10-25 na poschodie krat pocet poschodi
+		officeBuildings[i] = new OfficeBuilding(i + 7201, tempFloors, tempEmplyees, tempEmplyees * (rand() % 1000 + 100), rand() % 6 + 1); // departments: 1-6
+	}
+
+	for (int i = 0; i < numOfFactories; i++)
+	{
+		tempFloors = rand() % 2 + 1; // floors: 1-2
+		tempEmplyees = tempFloors * (rand() % 501 + 200); // employees: 200-700
+		factories[i] = new Factory(i + 7501, tempFloors, tempEmplyees, tempEmplyees * (rand() % 101 + 60), rand() % 3001 + 500); // area of production hall: 500-3500
+	}
+
+	cout << "Generating successfull" << endl;
+}
+
+void Street::printInfo()
+{
+	cout << "STREET NAME: " << streetName << "\nCITY: " << cityName << "\n\nFollowing buildings are located on this street:\n\n";
+
+	if (familyHouses != nullptr) // ak by to nebolo alokovane, tak nevypise nic
+	{
+		cout << "FAMILY HOUSES:\n";
+		for (int i = 0; i < numOfFamilyHouses; i++)
+			familyHouses[i]->printInfo();
+	}
+
+	if (blocksOfFlats != nullptr)
+	{
+		cout << "\nBLOCKS OF FLATS:\n";
+		for (int i = 0; i < numOfBlocksOfFlats; i++)
+			blocksOfFlats[i]->printInfo();
+	}
+
+	if (officeBuildings != nullptr)
+	{
+		cout << "\nOFFICE BUILDINGS:\n";
+		for (int i = 0; i < numOfOfficeBuildings; i++)
+			officeBuildings[i]->printInfo();
+	}
+
+	if (factories != nullptr)
+	{
+		cout << "\nFACTORIES:\n";
+		for (int i = 0; i < numOfFactories; i++)
+			factories[i]->printInfo();
+	}
+}
+
+int main()
+{
+	Street ulica("Racianska", "Bratislava");
+
+	cout << ulica.getCityName() << "  " << ulica.getStreetName() << endl;
+
+	ulica.generateBuildings();
+
+	ulica.printInfo();
+
+	//cout << "FH num: " << ulica.familyHouses[1]->getNumber() << ", floors: " << ulica.familyHouses[1]->getFloors() << ", house area: " << ulica.familyHouses[1]->getHouseArea() << " m^2, garden area: " << ulica.familyHouses[1]->getGardenArea() << " m^2" << endl;
+
+
+	return 0;
 }
